2a4fb73b8c394b2527d1a763cd42ccd6b3ec92fb
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-16 14:55:40 -0700

    refactor: :recycle: adding some style changes and adding metric function to make sizes responsive

--- src/components/atoms/Heading/index.jsx
+++ src/components/atoms/Heading/index.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types'
 import { Text } from 'react-native'
 import { styleSheet } from './styles'
 
-export default function Heading({ text, size, weight, color }) {
-  const styles = styleSheet({ size, weight, color })
+export default function Heading({ text, size, font, color }) {
+  const styles = styleSheet({ size, font, color })
   return <Text style={styles.HeadingStyle}>{text}</Text>
 }
 
 Heading.propTypes = {
   text: PropTypes.string.isRequired,
   size: PropTypes.number,
-  weight: PropTypes.string,
+  font: PropTypes.string,
   color: PropTypes.string,
 }
--- src/components/atoms/Heading/styles.js
+++ src/components/atoms/Heading/styles.js
@@ -1,15 +1,15 @@
 import { StyleSheet } from 'react-native'
-import { theme } from '../../../styles/theme'
+import { theme } from '../../../styles'
 
 export function styleSheet(props) {
-  const { size = 28, weight, color = theme.colors.white } = props
+  const { size = 28, font, color = theme.colors.white } = props
 
   return StyleSheet.create({
     HeadingStyle: {
-      fontSize: size,
-      fontWeight: weight,
       color: color,
-      marginVertical: 8,
+      marginVertical: theme.metrics.pixel(12),
+      fontSize: theme.metrics.pixel(size),
+      fontFamily: theme.fonts[font],
     },
   })
 }
