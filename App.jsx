64fe795fb54113b73103bf8eaa358819944dc608
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-16 12:49:52 -0700

    feat: :tada: initial commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import SplashScreen from "./src/screens/SplashScreen";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <SplashScreen />
+    </View>
+  );
+}
