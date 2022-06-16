27bcc667086597b59f60a66415a1f2f247f0e742
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-16 14:53:32 -0700

    chore: :heavy_plus_sign: adding app global font

--- App.jsx
+++ App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { View } from "react-native";
-import SplashScreen from "./src/screens/SplashScreen";
+import React from 'react'
+import { View } from 'react-native'
+import AppLoading from 'expo-app-loading'
+import {
+  useFonts,
+  SourceSansPro_700Bold,
+  SourceSansPro_400Regular,
+  SourceSansPro_900Black,
+  SourceSansPro_600SemiBold,
+} from '@expo-google-fonts/source-sans-pro'
+
+import SplashScreen from './src/screens/SplashScreen'
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    SourceSansPro_700Bold,
+    SourceSansPro_400Regular,
+    SourceSansPro_900Black,
+    SourceSansPro_600SemiBold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <SplashScreen />
     </View>
-  );
+  )
 }
