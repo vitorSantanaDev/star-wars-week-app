2799ea73ad36cf3b9fb9c01d452393fd63dd45f5
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-16 14:59:24 -0700

    refactor: :recycle: adding metric function to make sizes responsive

--- src/components/atoms/Logo/index.jsx
+++ src/components/atoms/Logo/index.jsx
@@ -4,11 +4,19 @@ import PropTypes from 'prop-types'
 import LogoEmpire from '../../../../assets/images/LogoEmpire.svg'
 
 import { styleSheet } from './styles'
+import { theme } from '~/styles'
 
 export default function Logo({ size }) {
   const styles = styleSheet({ size })
+  const sizeMetric = theme.metrics.pixel(size)
 
-  return <LogoEmpire style={styles.LogoStyle} width={size} height={size} />
+  return (
+    <LogoEmpire
+      width={sizeMetric}
+      height={sizeMetric}
+      style={styles.LogoStyle}
+    />
+  )
 }
 
 Logo.propTypes = {
