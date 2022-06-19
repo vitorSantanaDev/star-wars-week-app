233fa358ac6ca48cbf63c1d1d034f785a4ddbb1d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-18 17:54:21 -0700

    feat: :sparkles: adding SplashScreen functionality

--- src/screens/SplashScreen/index.jsx
+++ src/screens/SplashScreen/index.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { APP_TEXT } from '../../util/appTexts'
 
-import { Heading, Logo, ScreenWrapper } from '../../components'
+import { Heading, Logo, ScreenWrapper } from '~/components'
+import { routeName } from '~/routes/routesName'
 
-export default function SplashScreen() {
+export default function SplashScreen({ navigation }) {
+  function goToHomeScreen() {
+    setTimeout(() => {
+      navigation.navigate(routeName.HOME)
+    }, 3000)
+  }
+  useEffect(goToHomeScreen, [navigation])
+
   return (
     <ScreenWrapper alignItem="center" justifyContent="center">
-      <Logo size={64} />
+      <Logo size="large" />
       <Heading size={24} font="bold" text={APP_TEXT.SPLASH_SCREEN_TEXT} />
     </ScreenWrapper>
   )
--- src/screens/SplashScreen/styles.js
+++ src/screens/SplashScreen/styles.js
@@ -1,5 +1,4 @@
 import { StyleSheet } from 'react-native'
-import { theme } from '~/styles'
 
 export function styleSheet(props) {
   const {} = props
