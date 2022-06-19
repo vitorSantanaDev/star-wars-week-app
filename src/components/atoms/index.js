d135fe793aa126f82b281c8e588721093568dc77
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-06-18 17:47:04 -0700

    chore: :seedling: update in atoms folder imports file

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,3 +1,4 @@
-export { default as Heading } from './Heading'
 export { default as Logo } from './Logo'
+export { default as GoBack } from './GoBack'
+export { default as Heading } from './Heading'
 export { default as ScreenWrapper } from './ScreenWrapper'
